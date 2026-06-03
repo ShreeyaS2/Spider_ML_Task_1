81d692b2e5b961fcd41b04cea266d4d77a06a6f3
Shreeya Sinha <shreeyasinha27@gmail.com>
2026-06-03 17:27:26 +0530

    initial commit

---
+++ applied_ml_domain/rag_pipeline/rag.py
@@ -0,0 +1,124 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders import PyMuPDFLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+import streamlit as st
+
+PAPERS_DIR = r"C:\Personal\comp_proj\neuralnetwork\rag_pipeline\papers"
+CHROMA_DIR = r"C:\Personal\comp_proj\neuralnetwork\rag_pipeline\chroma_db"
+EMBED_MODEL = "BAAI/bge-small-en-v1.5"  
+
+#Ingesting papers and splitting into chunks
+def ingest_papers():
+    with st.spinner("Ingesting papers..."):
+        pdf_files = DirectoryLoader(PAPERS_DIR, glob="**/*.pdf", loader_cls=PyMuPDFLoader)
+        files = pdf_files.load()
+    if not files:
+        st.write(f"No PDFs found")
+        return []
+
+    papers = set()
+    for file in files:
+        name= os.path.basename(file.metadata.get("source", ""))
+        file.metadata["paper"] = name
+        papers.add(name)
+
+    st.write(f"Found {len(papers)} papers, {len(files)} pages. Splitting into chunks...")
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
+    chunks = splitter.split_documents(files) #splits a list of documents into smaller chunks based on the specified chunk size and overlap. Each chunk is treated as a separate document, allowing for more manageable processing in subsequent steps.
+    st.write(f"Total chunks created: {len(chunks)}")
+    return chunks
+
+#Embeddings
+@st.cache_resource
+def get_embeddings():
+    return HuggingFaceEmbeddings(model_name=EMBED_MODEL, encode_kwargs={"normalize_embeddings": True})
+
+#Building and loading vectorstore
+def build_vectorstore():
+    chunks = ingest_papers()
+    if not chunks:
+        return None
+    with st.spinner("Building vectorstore..."):
+        embeddings = get_embeddings()
+        vectorstore = Chroma.from_documents(documents=chunks, embedding=embeddings, persist_directory=CHROMA_DIR, collection_name="papers")
+    st.write("Vectorstore ready.\n")
+    return vectorstore
+
+def load_vectorstore():
+    with st.spinner("Loading vectorstore..."):
+        embeddings = get_embeddings()
+        return Chroma(collection_name="papers", persist_directory=CHROMA_DIR, embedding_function=embeddings)
+
+#Retrieve context
+def retrieve(vectorstore, query):
+    with st.spinner("Retrieving relevant chunks..."):
+        return vectorstore.similarity_search(query, k=3)
+
+#Generate answer
+def generate_answer(query, docs):
+    with st.spinner("Generating answer..."):
+        context_parts = []
+        for doc in docs:
+            paper = doc.metadata.get("paper", "unknown")
+            context_parts.append(f"[Source: {paper}]\n{doc.page_content}")
+        context = "\n\n".join(context_parts)
+
+        prompt = f"""You are a research assistant answering questions about NLP and LLM research papers. Use only the context below to answer in a clear, precise and educated manner. Mention the source paper(s) in your answer. If the context lacks sufficient information, say so directly and do not make up any information.
+
+    Context:
+    {context}
+
+    Question: {query}
+
+    Answer:"""
+        
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",max_tokens=4000, api_key="AQ.Ab8RN6Lxbma4gXHsL9Jku-Jc7XSp8tENmAl_xEYNBrt_p5yhvQ")
+        response = llm.invoke(prompt)
+        return response.content
+
+def run():
+    st.title("RAG Pipeline Application")
+    st.write("This application allows you to go through a collection of NLP research papers using a Retrieval-Augmented Generation (RAG) pipeline. It ingests PDF papers, splits them into chunks, creates embeddings, and retrieves relevant information to answer your queries.")
+    os.makedirs(PAPERS_DIR, exist_ok=True)
+    if "vectorstore" not in st.session_state:
+        if os.path.exists(CHROMA_DIR) and os.listdir(CHROMA_DIR):
+            st.session_state.vectorstore = load_vectorstore()
+        else:
+            st.session_state.vectorstore = build_vectorstore()
+            if st.session_state.vectorstore is None:
+                st.write(f"Please add PDF papers to the folder and run again.")
+                return
+
+    st.write("RAG Pipeline Application ready. Type 'quit' to exit.\n")
+
+    query = st.text_input("Query: ").strip()
+    
+    if query.lower() == "quit":
+        st.write("Exiting RAG Pipeline. Goodbye!")
+        return
+    elif query:
+        docs = retrieve(st.session_state.vectorstore, query)
+        answer = generate_answer(query, docs)
+
+        st.divider()
+        st.header("Answer:")
+        st.write(answer)
+        with st.expander("Show retrieved chunks"):
+            with st.spinner("Loading retrieved chunks..."):
+                st.header("\nRetrieved Chunks:")
+                for i, doc in enumerate(docs):
+                    paper = doc.metadata.get("paper", "unknown")
+                    page = doc.metadata.get("page", "unknown")
+                    excerpt = doc.page_content[:400].replace("\n", " ").strip()
+                    st.subheader(f"{i+1}. Paper: {paper}, Page: {page}")
+                    st.write(f"{excerpt}...")
+                    st.divider()
+
+
+
+if __name__ == "__main__":
+    run()
---
+++ bonus_task/autoencoder.py
@@ -0,0 +1,137 @@
+#Code
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+#For fixed initialisation
+SEED = 42
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+train_set = torchvision.datasets.FashionMNIST(root='./data', download=True, train=True, transform=transform)
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+
+test_set = torchvision.datasets.FashionMNIST(root='./data', download=True, train=False, transform=transform)
+test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
+
+DEVICE= torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#Autoencoder
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(784, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU()
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, 784),
+            nn.Sigmoid() 
+        )
+    
+    def forward(self, x):
+        encode=self.encoder(x)
+        decode=self.decoder(encode)
+        return decode
+    
+#Training
+def train(model, loader, criterion, optimizer, device):
+    model.train()
+
+    for images, _ in loader: #every 64 images and labels in the training set
+        images = images.to(device)
+        noise = torch.randn_like(images) * 0.05
+        noisy_images = images + noise
+        noisy_images = torch.clamp(noisy_images, 0.0, 1.0)
+        
+        optimizer.zero_grad()
+        logits = model.forward(noisy_images)
+        loss   = criterion(logits, images.reshape(-1, 784)) #compare output to original image (flattened)
+        loss.backward()
+        optimizer.step()
+
+#Evaluate(no gradient calculation)
+@torch.no_grad()
+def evaluate(model, loader, criterion, device):
+    model.eval()
+    total_loss = 0
+    for images, _ in loader:
+        images = images.to(device)
+
+        logits = model.forward(images)
+        loss   = criterion(logits, images.reshape(-1, 784))
+        total_loss += loss.item()*images.size(0)
+    avg_loss = total_loss / len(loader.dataset)
+    return images, logits, avg_loss        
+        
+model=Autoencoder().to(DEVICE)
+criterion=nn.BCELoss()
+
+optimizer=optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
+
+output=[]
+for epoch in range(30):
+    train(model, train_loader, criterion, optimizer, DEVICE)
+    images, logits, loss = evaluate(model, test_loader, criterion, DEVICE)       
+    print(f"Epoch {epoch+1}, Loss: {loss:.4f}")
+    output.append((images, logits)) #last batch value for every epoch
+
+# grab the last epoch's images and reconstructions
+images, reconstructed = output[-1]
+
+images        = images.detach().cpu()
+reconstructed = reconstructed.detach().cpu()
+
+#reconstructed is (B, 784) — needs reshaping
+reconstructed = reconstructed.reshape(-1, 1, 28, 28)
+
+fig, axes = plt.subplots(2, 10, figsize=(15, 3))
+fig.suptitle("Original vs Reconstructed", fontsize=13, fontweight="bold")
+
+for i in range(10):
+    # top row — original images
+    axes[0, i].imshow(images[i].squeeze(), cmap="gray")
+    axes[0, i].axis("off")
+    if i == 0:
+        axes[0, i].set_title("Original", loc="left")
+    
+    # bottom row — reconstructed images
+    axes[1, i].imshow(reconstructed[i].squeeze(), cmap="gray")
+    axes[1, i].axis("off")
+    if i == 0:
+        axes[1, i].set_title("Reconstructed", loc="left")
+
+plt.tight_layout()
+plt.savefig("reconstructions.png", dpi=150, bbox_inches="tight")
+plt.show()
+
